07c4587ef191d98f8bdc47cac2f07a4cc3ff5e52
Robert Edwards <rcedwards@gmail.com>
2014-06-01 21:32:18 -0400

    Add note regarding origin of this crazy regex.

--- Sandbox/Sandbox/NSString+Validation.h
+++ Sandbox/Sandbox/NSString+Validation.h
@@ -5,6 +5,9 @@
 //  Created by Robert Edwards on 3/7/14.
 //  Copyright (c) 2014 Panko. All rights reserved.
 //
+// NSString escaped version of perl based
+// RFC 822 email address regex described here: http://www.ex-parrot.com/pdw/Mail-RFC822-Address.html
+//
 
 #import <Foundation/Foundation.h>
 
